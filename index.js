af4185c71d559c906ba5fb18034fb8f76edc7c56
Sam Miyakawa <smiyakawa@lyft.com>
2020-10-23 22:56:45 -0700

    mvp

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.get("/withdraw/api/v1/links/:withdrawId", async (req, res) => {
+  try {
+    const { data } = await axios.get(
+      `https://lnbits.com/withdraw/api/v1/links/${req.params.withdrawId}`,
+      {
+        headers: { "X-Api-Key": process.env.LNBITS_READ_KEY },
+      }
+    );
+    const { lnurl, ...rest } = data;
+
+    res.json(rest);
+  } catch (error) {
+    res.status(error.response.status).json(error.response.data);
+  }
+});
+
+app.listen(port, () => console.log(`server started on port ${port}`));
