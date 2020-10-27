bde3851b868d056e6c38bd0c4436cb917042cd0c
Sam Miyakawa <smiyakawa@lyft.com>
2020-10-26 22:39:31 -0700

    Enable CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const pick = require("lodash/pick");
 
@@ -6,6 +7,17 @@ require("dotenv").config();
 
 const app = express();
 const port = 8080;
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (process.env.ALLOWED_DOMAINS.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("fuck outta here"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.get("/withdraw/api/v1/links/:withdrawId", async (req, res) => {
   try {
@@ -15,15 +27,8 @@ app.get("/withdraw/api/v1/links/:withdrawId", async (req, res) => {
         headers: { "X-Api-Key": process.env.LNBITS_READ_KEY },
       }
     );
-    const payload = req.query.fields
-      ? pick(data, req.query.fields.split(","))
-      : data;
 
-    res.json(
-      process.env.ALLOWED_DOMAINS.split(",").includes(req.host)
-        ? payload
-        : "fuck outta here"
-    );
+    res.json(req.query.fields ? pick(data, req.query.fields.split(",")) : data);
   } catch (error) {
     res.status(error.response.status).json(error.response.data);
   }
